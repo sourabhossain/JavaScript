28ca027fd7b12f9f842200779e240ed4c46ba18c
Sourab Hossain <sourabhossain.fci@gmail.com>
2021-10-01 01:16:43 +0600

    DEV: #05 - set value of a browser Cookie

--- killer_javaScript.js
+++ killer_javaScript.js
@@ -1,4 +1,10 @@
-// Get Value of a browser Cookie
+// get value of a browser Cookie
 const getCookie = (name) => `; ${document.cookie}`.split(`; ${name}=`).pop().split(';').shift();
 
-
+// set value of a browser Cookie
+const setCookie = (name, value, days) => {
+    const date = new Date();
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    const expires = `expires=${date.toGMTString()}`;
+    document.cookie = `${name}=${value}; ${expires}; path=/`;
+};
