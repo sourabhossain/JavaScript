338b6434086562c1fb196778a52329b78980f3ba
Sourab Hossain <sourabhossain.fci@gmail.com>
2021-10-01 01:48:34 +0600

    DEV: #05 - Convert Hex to RGB

--- killer_javaScript.js
+++ killer_javaScript.js
@@ -11,3 +11,13 @@ const setCookie = (name, value, days) => {
 
 // Convert RGB to Hex
 const rgbToHex = (r, g, b) => `#${((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1)}`;
+
+// Convert Hex to RGB
+const hexToRgb = (hex) => {
+    const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16)
+    } : null;
+};
