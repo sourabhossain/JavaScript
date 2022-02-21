7e641a7617ce157cf8f587f16816c1b346946b3c
Sourab Hossain <sourabhossain.fci@gmail.com>
2022-02-21 19:35:12 +0600

    Linked List

--- linkedList.js
+++ linkedList.js
@@ -34,6 +34,7 @@ class LinkedList {
         }
 
         this.tail = node;
+        this.length++;
         return this;
     }
 
@@ -62,6 +63,142 @@ class LinkedList {
         return this;
     }
 
+    delete(value) {
+        if (!this.head) {
+            return null;
+        }
+
+        if (this.head.value === value) {
+            this.head = this.head.next;
+            this.length--;
+            return this;
+        }
+
+        let currentNode = this.head;
+        let previousNode = null;
+
+        while (currentNode) {
+            if (currentNode.value === value) {
+                previousNode.next = currentNode.next;
+                this.length--;
+                return this;
+            }
+
+            previousNode = currentNode;
+            currentNode = currentNode.next;
+        }
+
+        return this;
+    }
+
+    deleteHead() {
+        if (!this.head) {
+            return null;
+        }
+
+        this.head = this.head.next;
+        this.length--;
+        return this;
+    }
+
+    deleteTail() {
+        if (!this.head) {
+            return null;
+        }
+
+        if (!this.head.next) {
+            this.head = null;
+            this.tail = null;
+            this.length--;
+            return this;
+        }
+
+        let currentNode = this.head;
+        let previousNode = null;
+
+        while (currentNode.next) {
+            previousNode = currentNode;
+            currentNode = currentNode.next;
+        }
+
+        this.tail = previousNode;
+        previousNode.next = null;
+        this.length--;
+
+        return this;
+    }
+
+    reverse() {
+        let node = this.head;
+        let previousNode = null;
+        let nextNode = null;
+
+        while (node) {
+            nextNode = node.next;
+            node.next = previousNode;
+            previousNode = node;
+            node = nextNode;
+        }
+
+        this.head = previousNode;
+        return this;
+    }
+
+    __reverseRecursively(node) {
+        if (!node || !node.next) {
+            this.head = node;
+            return node;
+        }
+
+        const nextNode = this.__reverseRecursively(node.next);
+        nextNode.next = node;
+        node.next = null;
+        return node;
+    }
+
+    reverseRecursively() {
+        this.tail = this.head;
+        this.__reverseRecursively(this.head);
+    }
+
+    find(value) {
+        let node = this.head;
+
+        while (node) {
+            if (node.value === value) {
+                return node;
+            }
+
+            node = node.next;
+        }
+
+        return null;
+    }
+
+    fromArray(values) {
+        values.forEach(value => {
+            this.append(value);
+        });
+
+        return this;
+    }
+
+    toArray() {
+        const values = [];
+        let node = this.head;
+
+        while (node) {
+            values.push(node.value);
+            node = node.next;
+        }
+
+        return values;
+    }
+
+    size() {
+        return this.length;
+    }
+
     print() {
         let node = this.head;
 
@@ -82,4 +219,9 @@ node.append(5);
 
 node.insert(6, -10);
 
+node.delete(2);
+node.deleteHead();
+node.deleteTail();
+
 node.print();
+console.log(node.toArray());
