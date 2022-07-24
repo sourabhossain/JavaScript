8e186d81ce5572d1fe210cc1b7b7815e6b62637e
Sourab Hossain <32600240+sourabhossain@users.noreply.github.com>
2022-07-24 10:03:31 +0600

    toggle full screen

---
+++ toggleFullScreen.js
@@ -0,0 +1,11 @@
+function toggleFullScreen() { 
+    if (!document.fullscreenElement) {   
+        document.documentElement.requestFullscreen(); 
+    } else {  
+        if (document.exitFullscreen) {   
+            document.exitFullscreen();  
+        } 
+    }
+}
+
+toggleFullScreen();
